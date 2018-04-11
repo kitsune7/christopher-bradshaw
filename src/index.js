34c48506c6f2d8500c7e98fe63e44e1c3d83aaa3
Christopher Bradshaw <chris.kofox@gmail.com>
2018-04-11 14:16:31 -0600

    Made into a functional nodejs server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import path from 'path'
+
+const NODE_ENV = process.env.PORT
+const PORT = NODE_ENV == "production"? 80: 3000
+
+const app = express()
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/../public/404.html')))
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
