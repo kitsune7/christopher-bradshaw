fe9f4dda6ad15485a4a337505e1c1d9cbaf2d8e3
Christopher Bradshaw <chris.kofox@gmail.com>
2018-07-28 03:32:25 -0600

    Added request forwarding for ttr

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import path from 'path'
+import request from 'request'
+
 
 const PORT = 3001
-
 const app = express()
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')))
+app.get('/ttr-demo', (req, res) => req.pipe(request('http://localhost:8081')).pipe(res))
 app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/../public/404.html')))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
