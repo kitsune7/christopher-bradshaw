3622d89677cd38025b250d4514e18ba7c45d3c17
Christopher Bradshaw <chris.kofox@gmail.com>
2018-07-30 13:25:13 -0600

    Fixed a bug with forwarding ttr stuff

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const app = express()
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')))
 app.get('/ttr-demo', (req, res) => req.pipe(request('http://localhost:8081')).pipe(res))
-app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/../public/404.html')))
+app.get('*', (req, res) => {
+  if (req.headers.referer === 'http://localhost:3001/ttr-demo') {
+    return req.pipe(request(`http://localhost:8081${req.url}`)).pipe(res)
+  }
+  return res.sendFile(path.join(__dirname + '/../public/404.html'))
+})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
