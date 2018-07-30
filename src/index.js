6370797434925b0eb07a70a2e4a600476014e9a1
Christopher Bradshaw <chris.kofox@gmail.com>
2018-07-30 22:00:48 +0000

    Separated ttr to its own subdomain

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ const PORT = 3001
 const app = express()
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')))
-app.get('/ttr-demo', (req, res) => req.pipe(request('http://localhost:8081')).pipe(res))
 app.get('*', (req, res) => {
-  if (req.headers.referer === 'http://localhost:3001/ttr-demo') {
-    return req.pipe(request(`http://localhost:8081${req.url}`)).pipe(res)
-  }
   return res.sendFile(path.join(__dirname + '/../public/404.html'))
 })
 
