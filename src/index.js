2df6c5fd886e24c2f69ee26ec50760db4d746758
Christopher Bradshaw <chris.kofox@gmail.com>
2018-09-13 02:56:32 -0600

    Removed babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import path from 'path'
-import request from 'request'
-
+const express = require('express')
+const path = require('path')
 
 const PORT = 3001
 const app = express()
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')))
 app.get('*', (req, res) => {
